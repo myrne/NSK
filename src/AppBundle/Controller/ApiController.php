a7862f2195ccc74acec1454d77107c9921fc2d77
Myrne Stol <myrne.stol@gmail.com>
2021-12-22 22:11:03 +0100

    Use AbstractFOSRestController
    From the release notes for FOSRestBundle 2.5.0:
    
    "deprecated the FOSRestController base class, use the new AbstractFOSRestController instead".

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -16,7 +16,7 @@ use AppBundle\Entity\ProductAttributeRelation;
 use AppBundle\Entity\ProductAttributeFile;
 use AppBundle\Entity\Attribute;
 
-class ApiController extends FOSRestController
+class ApiController extends AbstractFOSRestController
 {
     use UploadControllerTrait;
 
