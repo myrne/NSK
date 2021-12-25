3a28de0c609d676dc66b6228f45ce3fc6640d86c
Myrne Stol <myrne.stol@gmail.com>
2021-12-25 15:57:57 +0100

    Remove disabled code for HTTPCache
    It's not used right now and probably won't be used any time soon, given the nature of the Nexxus app.

--- web/app.php
+++ web/app.php
@@ -7,8 +7,6 @@ $loader = require __DIR__.'/../app/autoload.php';
 
 $kernel = new AppKernel('prod', false);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
